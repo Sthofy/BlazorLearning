8a9fe8e63449a391f46663c494365571c0a5d240
Sthofy <suhajdakristof@gmail.com>
2023-01-20 20:24:47 +0100

    L 4-24 Add Search Methods to the Client

--- BlazorEcommerce/BlazorEcommerce/Client/Services/ProductService/IProductService.cs
+++ BlazorEcommerce/BlazorEcommerce/Client/Services/ProductService/IProductService.cs
@@ -2,9 +2,12 @@
 {
     public interface IProductService
     {
+        public string Message { get; set; }
         event Action ProductChanged;
         List<Product> Products { get; set; }
         Task GetProducts(string? categoryUrl = null);
         Task<ServiceResponse<Product>> GetProductAsync(int productId);
+        Task SearchProducts(string searchText);
+        Task<List<string>> GetProductSeachSuggestions(string searchText);
     }
 }
--- BlazorEcommerce/BlazorEcommerce/Client/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/BlazorEcommerce/Client/Services/ProductService/ProductService.cs
@@ -3,6 +3,7 @@
     public class ProductService : IProductService
     {
         private readonly HttpClient _http;
+        public string Message { get; set; } = "Loading products...";
 
         public ProductService(HttpClient http)
         {
@@ -30,6 +31,28 @@
                 Products = result.Data;
 
             ProductChanged.Invoke();
+        }
+
+        public async Task<List<string>> GetProductSeachSuggestions(string searchText)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<string>>>($"api/product/searchsuggestions/{searchText}");
+
+            return result.Data;
+        }
+
+        public async Task SearchProducts(string searchText)
+        {
+            var result = await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/search/{searchText}");
+
+            if (result != null && result.Data != null)
+            {
+                Products = result.Data;
+            }
+
+            if (Products.Count == 0)
+                Message = "No products found.";
+            else
+                ProductChanged?.Invoke();
         }
     }
 }
