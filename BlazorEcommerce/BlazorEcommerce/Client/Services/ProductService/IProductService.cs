d2c01a94e15462a8643f7d09f2d918e48cee6645
Sthofy <suhajdakristof@gmail.com>
2023-01-27 15:47:43 +0100

    L 4-34 Add Pagination to the ProductService on the Client

--- BlazorEcommerce/BlazorEcommerce/Client/Services/ProductService/IProductService.cs
+++ BlazorEcommerce/BlazorEcommerce/Client/Services/ProductService/IProductService.cs
@@ -2,12 +2,15 @@
 {
     public interface IProductService
     {
-        public string Message { get; set; }
+        string Message { get; set; }
+        int CurrentPage { get; set; }
+        int PageCount { get; set; }
+        string LastSearchText { get; set; }
         event Action ProductChanged;
         List<Product> Products { get; set; }
         Task GetProducts(string? categoryUrl = null);
         Task<ServiceResponse<Product>> GetProductAsync(int productId);
-        Task SearchProducts(string searchText);
+        Task SearchProducts(string searchText, int page);
         Task<List<string>> GetProductSeachSuggestions(string searchText);
     }
 }
