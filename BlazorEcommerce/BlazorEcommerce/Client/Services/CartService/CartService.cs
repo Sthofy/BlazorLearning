1e549796895ffc2154f8dc021032b4fa161473c8
Sthofy <suhajdakristof@gmail.com>
2023-01-27 17:18:32 +0100

    L 4-41 Show the Number of Items in the Cart Counter

--- BlazorEcommerce/BlazorEcommerce/Client/Services/CartService/CartService.cs
+++ BlazorEcommerce/BlazorEcommerce/Client/Services/CartService/CartService.cs
@@ -25,6 +25,8 @@ namespace BlazorEcommerce.Client.Services.CartService
             cart.Add(cartItem);
 
             await _localStorage.SetItemAsync("cart", cart);
+
+            OnChange.Invoke();
         }
 
         public async Task<List<CartItem>> GetCartItems()
