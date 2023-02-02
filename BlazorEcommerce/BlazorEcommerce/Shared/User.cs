8cf8906b9e41a099db038601089104106845331c
Sthofy <suhajdakristof@gmail.com>
2023-02-02 17:11:37 +0100

    L 8-01 Add Address Model & New Migration

--- BlazorEcommerce/BlazorEcommerce/Shared/User.cs
+++ BlazorEcommerce/BlazorEcommerce/Shared/User.cs
@@ -7,6 +7,7 @@
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
         public DateTime DateCreated { get; set; } = DateTime.Now;
+        public Address Address { get; set; }
 
     }
 }
